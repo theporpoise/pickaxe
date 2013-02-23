70d22e5159c42466ec269552df9ec3b75fae17db
Matthew Gould <matt@curebit.com>
2013-02-23 12:16:14 -0500

    Control Structrues

--- one.rb
+++ one.rb
@@ -71,12 +71,47 @@ section = {
 
 puts "An oboe is a #{section[:oboe]}"
 
+# Control Structures
 
+=begin
+puts "please enter a number"
+count = gets.chomp().to_i
+tries = 0
 
+if count > 10
+  puts "Try again"
+elsif tries == 3
+  puts "You lose"
+else
+  puts "Enter a number"
+end
 
+while weight < 100 and num_pallets <= 30
+  pallet = next_pallet()
+  weight = += pallet.weight
+  num_pallets +=1
+end
 
+while line = gets
+  puts line.downcase
+end
 
+if radiation > 3000
+  puts "Danger, Will Robinson"
+end
 
+puts "Danger, Will Robinson" if radiation > 3000
+
+square = 2
+while square < 1000
+  square = square * square
+end
+
+square = 2
+square = square * square while square < 1000
+=end
+
+# Regular Expressions
 
 
 

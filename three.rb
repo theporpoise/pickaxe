36f785330c28c52a1b086ada636d904fe4d90c2d
Matthew Gould <matt@curebit.com>
2013-02-23 14:50:18 -0500

    created a virtual instance variable, which is essentially a calculated instance variable, that is actually derived from one of the real instance variables that is set when it is initialized.  The good thing is, that to the outside world, this virtual instance variable looks and behaves like the real thing, so they don't have to worry about how it's implemented in my particular class, they can use it however.

--- three.rb
+++ three.rb
@@ -6,11 +6,21 @@ class BookInStock
     @price = Float(price)
   end
 
+  def price_in_cents
+    Integer(price*100 + 0.5)
+  end
+  def price_in_cents=(cents)
+    @price = cents / 100.0
+  end
+
   def to_s
     "ISBN: #{@isbn}, price: #{@price}"
   end
 end
 
+book = BookInStock.new("isbn1", 33.8)
+
+=begin
 b1 = BookInStock.new("isbn1", 3)
 b2 = BookInStock.new("isbn2", 3.14)
 b3 = BookInStock.new("isbn3", "5.67")
@@ -20,6 +30,7 @@ p b3
 puts b1
 puts b2
 puts b3
+=end
 
 =begin
 
@@ -33,6 +44,7 @@ puts b3
 # instance is a area of places :)  when you define the to_s method  it also affects p
 =end
 
+=begin
 # Objects and Attributes
 book = BookInStock.new("isbn10", 12.34)
 puts "ISBN  = #{book.isbn}"
@@ -46,6 +58,16 @@ puts "ISBN    = #{abook1.isbn}"
 puts "Price   = #{abook1.price}"
 abook1.price = abook1.price * 0.75  # discount price
 puts "New Price = #{abook1.price}"
+=end
+
+#Virtual Attributes
+puts "Virtual Attributes"
+puts " Price          = #{book.price}"
+puts " Price in cents = #{book.price_in_cents}"
+book.price_in_cents = 1234
+puts "#{book.price}"
+puts "#{book.price_in_cents}"
+
 
 
 

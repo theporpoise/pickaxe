0f2fb363f8453253c291928027c9e53443e58a93
Matthew Gould <matt@curebit.com>
2013-02-22 20:35:44 -0500

    moving along

--- one.rb
+++ one.rb
@@ -24,5 +24,81 @@ end
 puts again_goodnight('Pa')
 puts again_goodnight('uncle')
 
+a = [ 1, 'cat', 3.14 ] # array with three elements
+puts "The first element is #{a[0]}"
+
+a[2] = nil
+
+puts "The array is now #{a.inspect}"
+
+a = [ 'ant', 'bee', 'cat', 'dog', 'elk' ]
+puts a[0]
+puts a[3]
+
+a = %w{ ant bee cat dog elk }
+puts a[0]
+puts a[3]
+
+inst_section = {
+    'cello' => 'string',
+    'clarinet' => 'woodwind',
+    'drum' => 'percussion',
+    'oboe' => 'woodwind',
+    'trumpet' => 'brass',
+    'violin' => 'string'
+}
+
+p inst_section['oboe']
+p inst_section['cello']
+p inst_section['bassoon']
+
+histogram = Hash.new(0)
+p histogram['ruby']
+p histogram['ruby'] = histogram['ruby'] + 1
+p histogram['ruby']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

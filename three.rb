a7e77310d6d076e723f5910864d40a06acde2aff
Matthew Gould <matt@curebit.com>
2013-02-23 13:40:42 -0500

    re-learned teh attr_reader call with the def initialize method in a ruby class.  forgot how useful that was :).  glad I reviewed :).  all attribute accessor methods defined in the attr_reader user a symbol, like :isbn and this becomes the method .isbn on the class object, in this case BookInStock.isbn returns the value of teh isbn instance variable.

--- three.rb
+++ three.rb
@@ -1,8 +1,10 @@
 class BookInStock
+  attr_reader :isbn, :price
   def initialize(isbn, price)
     @isbn = isbn
     @price = Float(price)
   end
+
   def to_s
     "ISBN: #{@isbn}, price: #{@price}"
   end
@@ -18,6 +20,8 @@ puts b1
 puts b2
 puts b3
 
+=begin
+
 # puts writes strings to the standard output, so when you puts an object it simply writes
 # the name of the object's class, followed bya  colon, and the objects uniue identifier
 # and it sticks that inside of #<> lol, that simple.  this is the default to_s method
@@ -26,3 +30,47 @@ puts b3
 # are variables like parameters which disappear quickly.  instance variables stay with
 # the instance :).  local is more temporary than instance.  local is a very specific place
 # instance is a area of places :)  when you define the to_s method  it also affects p
+=end
+
+# Objects and Attributes
+book = BookInStock.new("isbn10", 12.34)
+puts "ISBN  = #{book.isbn}"
+puts "Price = #{book.price}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

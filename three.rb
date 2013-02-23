052a16ac14c989e55964c2a629938c1ae010f50b
Matthew Gould <matt@curebit.com>
2013-02-23 13:31:55 -0500

    Classes, Objects, and Variables - define a class using Class CamelCaseTheClass, use a def initialize(params) call to initialize your object, use def to_s to define the string call on your object so it doesn't just print the object name and unique identifier or parameters with just p - you can specify what it prints and access all the instance variables that were assigned in the initialize method (or assigned anywhere else for that matter

---
+++ three.rb
@@ -0,0 +1,28 @@
+class BookInStock
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = Float(price)
+  end
+  def to_s
+    "ISBN: #{@isbn}, price: #{@price}"
+  end
+end
+
+b1 = BookInStock.new("isbn1", 3)
+b2 = BookInStock.new("isbn2", 3.14)
+b3 = BookInStock.new("isbn3", "5.67")
+p b1
+p b2
+p b3
+puts b1
+puts b2
+puts b3
+
+# puts writes strings to the standard output, so when you puts an object it simply writes
+# the name of the object's class, followed bya  colon, and the objects uniue identifier
+# and it sticks that inside of #<> lol, that simple.  this is the default to_s method
+# that you can then program to do something useful!  instance variables are assigned
+# to the object and are available to all methods of that object.  'local' variables
+# are variables like parameters which disappear quickly.  instance variables stay with
+# the instance :).  local is more temporary than instance.  local is a very specific place
+# instance is a area of places :)  when you define the to_s method  it also affects p

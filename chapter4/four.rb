192ad7ee892939f5c1538e646fec5ccc6a0763e4
Matthew Gould <matt@curebit.com>
2013-02-28 11:41:55 -0500

    ok, not only can you pass a block of code to a function, and then have that function call it with yield, you can also define that block of code to have a parameter, and the function can then pass parameters to the block simply by doing yield param1 param2 etc.  (not sure if you need a comma seperated)

--- chapter4/four.rb
+++ chapter4/four.rb
@@ -171,6 +171,15 @@ def three_times
 end
 three_times {puts "Hello"}
 
+def fib_up_to(max)
+  i1, i2 = 1, 1 #parallel assignment
+  while i1 <= max
+    yield i1
+    i1, i2 = i2, i1+i2
+  end
+end
+fib_up_to(1000) {|f| print f, ","}
+
 
 
 

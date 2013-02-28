601e683871a980c1bdca88f44206933279a6c99f
Matthew Gould <matt@curebit.com>
2013-02-28 09:22:31 -0500

    push and pop create a LIFO (last in first out, not last in last out, lol), shift and push and you have a FIFO, first in first out, because push adds to the end and shift removes from the beginning.

--- chapter4/four.rb
+++ chapter4/four.rb
@@ -3,9 +3,7 @@ Containers, Blocks, and Iterators
 
 Arrays:
 
-=end
 
-=begin
 a = [ 3.14159, "pie", 99]
 p a.class
 p a.length
@@ -25,8 +23,6 @@ a = [1, 3, 5, 7, 9]
 p a[-1]
 p a[-2]
 p a[-99]
-=end
-=begin
 a = [1, 3, 5, 7, 9]
 p a[1, 3]
 p a[3, 1]
@@ -45,9 +41,7 @@ p a
 
 a[1, 0] = ['steven', 'bob', 'alex']
 p a
-=end
 
-=begin
 creating a stack queue
 stack = []
 stack.push "red"
@@ -58,11 +52,15 @@ puts stack.pop
 puts stack.pop
 puts stack.pop
 p stack
+
 =end
 
-
-
-
+#unshift and shift to create a FIFO queue
+queue = []
+queue.push "red"
+queue.push "green"
+puts queue.shift
+puts queue.shift
 
 
 

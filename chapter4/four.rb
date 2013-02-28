7064551c2bfd9630b5e4111b44c43d067b3b5b4d
Matthew Gould <matt@curebit.com>
2013-02-28 09:55:18 -0500

    ok, wrote a little program here.  first we took a string and converted it to downcase, .downcase and then chopped it up into words using .scan(/[\w']+/) where that regex matches all word characters and the ' character.  I would simplify it to .scan(/[\w]+/) b/c who cares about the ' ? right?  lol.  Ok.  Then, we got the frequency of each by creating a new hash with default value 0 (using Hash.new(0) so that it's not nil) and for the word in word_list += 1 to the hash.  then we return the hash.  Finally, we sorted teh hash using .sort_by and passing in a block {|word, count| count} which knew to sort it by count (the value). So it then worked.  Then we used File.read("para.txt") to read in som text, sent it to the string to array converter using .downcase.scan(regex),  then on the the hash counter with the Hash.new(0), and then through the sorter, and then print it out.  It sorted it in ascending order.

--- chapter4/four.rb
+++ chapter4/four.rb
@@ -89,7 +89,33 @@ p h
 =end
 
 
+def words_from_string(string)
+  string.downcase.scan(/[\w']+/)
+end
 
+#p words_from_string("But I didn't inhale, he said (emphaticaly)")
+
+def count_frequency(word_list)
+  counts = Hash.new(0)
+  for word in word_list
+    counts[word] += 1
+  end
+  counts
+end
+#p count_frequency(words_from_string("But I didn't inhale, he said (emphatically)"))
+#p count_frequency(words_from_string("Sparky the cat sat on the mat"))
+
+raw_text = File.read("para.txt")
+word_list = words_from_string(raw_text)
+counts = count_frequency(word_list)
+sorted = counts.sort_by {|word, count| count}
+top_five = sorted.last(5)
+for i in 0...5
+  word = top_five[i][0]
+  count = top_five[i][1]
+  puts "#{word}: #{count}"
+end
+p sorted
 
 
 

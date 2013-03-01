e727330f779a2be77d7444f7d4b0961accc7ccdf
Matthew Gould <matt@curebit.com>
2013-03-01 11:57:09 -0500

    blocks have all the same abilities as methods now in Ruby 1.9.  Which is awesome.  they can take an arguement, an arguement list - something*, and a block, &someblock like so - procedure = -> parameter, parameterlist*, &someblock
    it gets called like this
    
    procedure.call('first_parameter', 'rest', 'of', 'parameters') { puts 'the blcok }

--- chapter4/four.rb
+++ chapter4/four.rb
@@ -432,6 +432,21 @@ end
 =end
 
 
+proc1 = lambda do |a, *b, &block|
+  puts "a = #{a.inspect}"
+  puts "b = #{b.inspect}"
+  block.call
+end
+
+proc1.call(1, 2, 3, 4) { puts "in block1"}
+
+proc2 = -> a, *b, &block do
+  puts "a = #{a.inspect}"
+  puts "hello = #{b.inspect}"
+  block.call
+end
+
+proc2.call('cat', 'dog', 'mouse', 'traffic') {puts "the block block"}
 
 
 

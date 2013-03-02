cc4032a4cf44058ea74c55f61eb9c8c255f71d87
Matthew Gould <matt@curebit.com>
2013-03-02 12:57:46 -0500

    most have a digit on both sides of decimal otherwise ruby tries to invoke the method.

--- chapter6/six.rb
+++ chapter6/six.rb
@@ -1,1 +1,16 @@
 # STandard Types - chapter 6 - stuff i prolly know :)
+
+num = 81
+6.times do
+  puts "#{num.class}: #{num}"
+  num *=  num
+end
+
+p 123456
+p 0d123456
+p 123_456
+p -543
+p 0xaabb
+p 0377
+p -0b10_1010
+p 123_456_789

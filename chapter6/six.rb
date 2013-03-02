281da7d73a7d7641e1ccdb55ebfde6fb01b6c669
Matthew Gould <matt@curebit.com>
2013-03-02 13:01:25 -0500

    supports fractions and complex numbers, which is cool.  prolly won't use, but neat that they support it.  Ruby is not really a math language - sorry ruby - i'll stick with python and r for the maths - actually, just r :)

--- chapter6/six.rb
+++ chapter6/six.rb
@@ -14,3 +14,9 @@ p 0xaabb
 p 0377
 p -0b10_1010
 p 123_456_789
+
+p Rational(3, 4) * Rational(2, 3)
+p Rational("3/4") * Rational("2/3")
+
+p Complex(1, 2) * Complex(3, 4)
+p Complex("1+2i") * Complex("3+4i")
